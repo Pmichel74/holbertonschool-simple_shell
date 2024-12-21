d1b5cf913d3204358ceb487e74e819fa0ea1ec77
jbn179 <9772@holbertonstudents.com>
2024-12-21 11:26:35 +0100

    fixed compilation

--- read_command.c
+++ read_command.c
@@ -17,7 +17,7 @@ char *read_command(void)
 	size_t bufsize = 0;
 	ssize_t characters;
 
-	printf("#my_simple_shell_$ ");
+	printf("$ ");
 	fflush(stdout);
 
 	characters = getline(&buffer, &bufsize, stdin);
