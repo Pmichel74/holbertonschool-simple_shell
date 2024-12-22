ecfe20b4145b891c5cb905afde860c86ead13ac5
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-22 17:24:01 +0100

    update task 0.1

--- main.h
+++ main.h
@@ -3,13 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
-#define MAX_COMMAND_LENGTH 100
-
-int main(void);
+int main(int argc, char *argv[]);
 
 #endif /* MAIN_H */
