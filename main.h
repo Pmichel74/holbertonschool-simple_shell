80e901e0a018964aa0cec0f5691c8896a3bae4f0
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-22 15:10:08 +0100

    add main.h

--- main.h
+++ main.h
@@ -5,18 +5,29 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+<<<<<<< HEAD
 #include <errno.h>
+=======
+>>>>>>> 611470c (add main.h)
 #include <sys/wait.h>
 #include <sys/stat.h>
 
 #define MAX_ARGS 64
 
+<<<<<<< HEAD
 
 int main(int argc, char *argv[], char *envp[]);
 
 char *read_command(void);
 int fork_and_execute(char *command_path, char **args, char **envp);
 int execute_command(char **args, char **envp, char *program_name);
+=======
+int main(int argc, char *argv[], char *envp[]);
+
+char *read_command(void);
+void fork_and_execute(char *command_path, char **args, char **envp);
+void execute_command(char **args, char **envp);
+>>>>>>> 611470c (add main.h)
 
 char **tokenize_command(char *command);
 void free_tokens(char **tokens);
