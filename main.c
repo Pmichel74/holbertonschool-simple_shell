73afd6af57963d8994b2ed204055c54899c8fda0
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-22 15:36:40 +0100

    update task 0.1

--- main.c
+++ main.c
@@ -1,11 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
-#define MAX_COMMAND_LENGTH 100
+#include "main.h"
 
 /**
  * main - Simple UNIX command line interpreter
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 100
+
+int main(void);
+
+#endif
