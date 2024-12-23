306251609de708c63c320e35b5e34db18e86d3d9
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-23 20:37:26 +0100

    updates non interactive

--- main.c
+++ main.c
@@ -11,51 +11,53 @@
 
 int main(int argc, char *argv[], char *envp[])
 {
-	char *command = NULL;
-	char **args;
-	int should_exit = 0;
-	int interactive = isatty(STDIN_FILENO);
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    char **args;
+    int interactive = isatty(STDIN_FILENO);
 
-	(void)argc;
+    (void)argc;  /* Unused parameter*/
 
-	while (!should_exit)
-	{
-		if (interactive)
-            write(STDOUT_FILENO, "($) ", 4);
+    while (1)
+    {
+        if (interactive)
+            printf("$ ");
 
-		command = read_command();
-		if (command == NULL)
-		{
-			if (interactive)
-                write(STDOUT_FILENO, "\n", 1);
+        nread = getline(&line, &len, stdin);
+        if (nread == -1)
+        {
+            if (interactive)
+                printf("\n");
             break;
-		}
-		if (strlen(command) == 0)
-		{
-			free(command);
-			continue;
-		}
-		args = tokenize_command(command);
-		if (!args)
-		{
-			free(command);
-			continue;
-		}
-		if (args[0] == NULL)
-		{
-			free_args(args);
-			free(command);
-			continue;
-		}
-		if (strcmp(args[0], "exit") == 0)
-			should_exit = 1;
-		else
-			execute_command(args, envp, argv[0]);
-		free_args(args);
-		free(command);
+        }
 
-		if (!interactive)
-            should_exit = 1;
-	}
-	return (0);
+        if (nread > 0 && line[nread - 1] == '\n')
+            line[nread - 1] = '\0';  /*Remove newline character*/
+
+        if (strlen(line) == 0)
+            continue;
+
+        args = tokenize_command(line);
+        if (!args)
+            continue;
+
+        if (args[0] == NULL)
+        {
+            free_args(args);
+            continue;
+        }
+
+        if (strcmp(args[0], "exit") == 0)
+        {
+            free_args(args);
+            break;
+        }
+
+        execute_command(args, envp, argv[0]);
+        free_args(args);
+    }
+
+    free(line);
+    return (0);
 }
