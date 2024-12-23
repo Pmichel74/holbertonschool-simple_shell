45accda47dc0d265906d972e9053f2547ab0cf8e
jbn179 <9772@holbertonstudents.com>
2024-12-23 22:25:45 +0100

    fixed betty

--- main.c
+++ main.c
@@ -9,55 +9,46 @@
  * Return: Always returns 0 (success)
  */
 
-int main(int argc, char *argv[], char *envp[])
+int main(int argc __attribute__((unused)), char *argv[], char *envp[])
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    char **args;
-    int interactive = isatty(STDIN_FILENO);
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	char **args;
+	int interactive = isatty(STDIN_FILENO);
 
-    (void)argc;  /* Unused parameter*/
+	while (1)
+	{
+		if (interactive)
+			printf("$ ");
 
-    while (1)
-    {
-        if (interactive)
-            printf("$ ");
-
-        nread = getline(&line, &len, stdin);
-        if (nread == -1)
-        {
-            if (interactive)
-                printf("\n");
-            break;
-        }
-
-        if (nread > 0 && line[nread - 1] == '\n')
-            line[nread - 1] = '\0';  /*Remove newline character*/
-
-        if (strlen(line) == 0)
-            continue;
-
-        args = tokenize_command(line);
-        if (!args)
-            continue;
-
-        if (args[0] == NULL)
-        {
-            free_args(args);
-            continue;
-        }
-
-        if (strcmp(args[0], "exit") == 0)
-        {
-            free_args(args);
-            break;
-        }
-
-        execute_command(args, envp, argv[0]);
-        free_args(args);
-    }
-
-    free(line);
-    return (0);
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
+		{
+			if (interactive)
+				printf("\n");
+			break;
+		}
+		if (nread > 0 && line[nread - 1] == '\n')
+			line[nread - 1] = '\0';  /*Remove newline character*/
+		if (strlen(line) == 0)
+			continue;
+		args = tokenize_command(line);
+		if (!args)
+			continue;
+		if (args[0] == NULL)
+		{
+			free_args(args);
+			continue;
+		}
+		if (strcmp(args[0], "exit") == 0)
+		{
+			free_args(args);
+			break;
+		}
+		execute_command(args, envp, argv[0]);
+		free_args(args);
+	}
+	free(line);
+	return (0);
 }
