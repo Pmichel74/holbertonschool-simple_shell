411277ec9b4f7b0765497157cb736a1241aeb93d
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-23 17:12:25 +0100

    fix output

---
+++ find_path.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * find_path - Searches for the PATH in environment variables
+ * @envp: Array of environment variables
+ *
+ * Return: A string containing the value of PATH, or NULL if not found
+ */
+
+char *find_path(char *envp[])
+{
+	int i;
+
+	for (i = 0; envp[i] != NULL; i++)
+	{
+		if (strncmp(envp[i], "PATH=", 5) == 0)
+			return (envp[i] + 5); /*Retourne la valeur après "PATH="*/
+	}
+	return (NULL);
+}
