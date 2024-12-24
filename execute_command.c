aa1a1230e07acfe387653110c65f5a11eeab8e75
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-24 17:35:03 +0100

    fix fork condition

--- execute_command.c
+++ execute_command.c
@@ -2,8 +2,6 @@
 #include <sys/stat.h>
 #include <errno.h>
 
-int forking_helper(char *command_path, char **args, char **envp);
-
 /**
  * execute_command - Executes a command with given arguments
  * @args: Array of command arguments
@@ -41,48 +39,4 @@ int execute_command(char **args, char **envp, char *program_name)
 	}
 
 	return (forking_helper(command_path, args, envp));
-}
-
-/**
- * forking_helper - Forks and executes a command
- * @command_path: Full path to the command
- * @args: Array of command arguments
- * @envp: Array of environment variables
- *
- * Return: 0 on success, -1 on failure
- */
-int forking_helper(char *command_path, char **args, char **envp)
-{
-	pid_t pid;
-	int status;
-
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("Error: fork failed");
-		free(command_path);
-		return (-1);
-	}
-	else if (pid == 0)
-	{
-		if (execve(command_path, args, envp) == -1)
-		{
-			perror("Error: execve failed");
-			free(command_path);
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		if (wait(&status) == -1)
-		{
-			perror("Error: wait failed");
-			free(command_path);
-			return (-1);
-		}
-		free(command_path);
-		if (WIFEXITED(status))
-			return (WEXITSTATUS(status));
-	}
-	return (-1);
 }
--- fork_and_execute.c
+++ fork_and_execute.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * fork_and_execute - Forks a new process and executes a command
- * @command_path: Full path to the command to be executed
- * @args: NULL-terminated array of command arguments
+ * forking_helper - Forks and executes a command
+ * @command_path: Full path to the command
+ * @args: Array of command arguments
  * @envp: Array of environment variables
  *
  * Return: 0 on success, -1 on failure
  */
-int fork_and_execute(char *command_path, char **args, char **envp)
+int forking_helper(char *command_path, char **args, char **envp)
 {
 	pid_t pid;
 	int status;
@@ -17,6 +17,7 @@ int fork_and_execute(char *command_path, char **args, char **envp)
 	if (pid == -1)
 	{
 		perror("Error: fork failed");
+		free(command_path);
 		return (-1);
 	}
 	else if (pid == 0)
@@ -24,6 +25,7 @@ int fork_and_execute(char *command_path, char **args, char **envp)
 		if (execve(command_path, args, envp) == -1)
 		{
 			perror("Error: execve failed");
+			free(command_path);
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -32,8 +34,10 @@ int fork_and_execute(char *command_path, char **args, char **envp)
 		if (wait(&status) == -1)
 		{
 			perror("Error: wait failed");
+			free(command_path);
 			return (-1);
 		}
+		free(command_path);
 		if (WIFEXITED(status))
 			return (WEXITSTATUS(status));
 	}
