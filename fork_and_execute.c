3e13c507c50dfc208f99daae3271e6ffd2857c04
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-24 10:59:57 +0100

    new command fork

--- fork_and_execute.c
+++ fork_and_execute.c
@@ -7,42 +7,44 @@
  * @args: NULL-terminated array of command arguments
  * @envp: Array of environment variables
  *
- * Return: 0 on success, -1 on failure
+ * Return: Exit status of the child process, or -1 on failure
  */
 int fork_and_execute(char *command_path, char **args, char **envp)
 {
-	pid_t pid = fork();
+	pid_t pid;
 	int status;
 
+	pid = fork();
 	if (pid == -1)
 	{
-		perror("Error: fork failed");
+		perror("fork");
 		return (-1);
 	}
 
 	if (pid == 0)
 	{
+		/* Child process */
 		if (execve(command_path, args, envp) == -1)
 		{
-			perror("Error:");
+			perror("execve");
 			exit(EXIT_FAILURE);
 		}
 	}
 	else
 	{
-		if (wait(&status) == -1)
+		/* Parent process */
+		if (waitpid(pid, &status, 0) == -1)
 		{
-			perror("Error: wait failed");
+			perror("waitpid");
 			return (-1);
 		}
+
 		if (WIFEXITED(status))
-		{
 			return (WEXITSTATUS(status));
-		}
-		else
-		{
-			return (-1);
-		}
+		if (WIFSIGNALED(status))
+			return (128 + WTERMSIG(status));
+		return (-1);
 	}
-	return (0);
+
+	return (0); /* This line should never be reached */
 }
