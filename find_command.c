a055dfe18a79562d78da18e884b78df578df1ebf
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-24 16:07:26 +0100

    change fork func

--- find_command.c
+++ find_command.c
@@ -1,28 +1,34 @@
 #include "main.h"
 
 /**
- * find_command - Searches for a command in PATH
- * or as an absolute/relative path
+ * find_command - Searches for a command in PATH or as absolute/relative path
  * @command: Name of the command to search for
  * @envp: Array of environment variables
  *
- * Return: The full path of the command if found, NULL otherwise
+ * Return: Full path of the command if found, NULL otherwise
  */
-
 char *find_command(char *command, char *envp[])
 {
 	char *path;
 	char *absolute_path;
 
-	absolute_path = check_absolute_path(command);
+	if (!command)
+		return (NULL);
 
+	absolute_path = check_absolute_path(command);
 	if (absolute_path)
 		return (absolute_path);
 
 	path = find_path(envp);
-
 	if (!path)
 		return (NULL);
 
-	return (search_in_path(command, path));
+	absolute_path = search_in_path(command, path);
+	if (!absolute_path)
+	{
+		fprintf(stderr, "%s: command not found\n", command);
+		return (NULL);
+	}
+
+	return (absolute_path);
 }
