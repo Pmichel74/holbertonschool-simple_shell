868cbb7e062354124a594e6b60f56e6933a53e57
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-24 17:27:07 +0100

    fix fork condition

--- execute_command.c
+++ execute_command.c
@@ -1,4 +1,8 @@
 #include "main.h"
+#include <sys/stat.h>
+#include <errno.h>
+
+int forking_helper(char *command_path, char **args, char **envp);
 
 /**
  * execute_command - Executes a command with given arguments
@@ -10,7 +14,7 @@
  */
 int execute_command(char **args, char **envp, char *program_name)
 {
-	char *command_path;
+	char *command_path = NULL;
 	struct stat st;
 
 	if (!args || !args[0])
@@ -26,11 +30,6 @@ int execute_command(char **args, char **envp, char *program_name)
 	{
 		if (stat(args[0], &st) == 0 && st.st_mode & S_IXUSR)
 			command_path = strdup(args[0]);
-		else
-		{
-			fprintf(stderr, "%s: 1: %s: not found\n", program_name, args[0]);
-			return (-1);
-		}
 	}
 	else
 		command_path = find_command(args[0], envp);
@@ -41,13 +40,49 @@ int execute_command(char **args, char **envp, char *program_name)
 		return (-1);
 	}
 
-	if (fork_and_execute(command_path, args, envp) == -1)
+	return (forking_helper(command_path, args, envp));
+}
+
+/**
+ * forking_helper - Forks and executes a command
+ * @command_path: Full path to the command
+ * @args: Array of command arguments
+ * @envp: Array of environment variables
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int forking_helper(char *command_path, char **args, char **envp)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == -1)
 	{
-		fprintf(stderr, "%s: 1: %s: %s\n", program_name, args[0], strerror(errno));
+		perror("Error: fork failed");
 		free(command_path);
 		return (-1);
 	}
-
-	free(command_path);
-	return (0);
+	else if (pid == 0)
+	{
+		if (execve(command_path, args, envp) == -1)
+		{
+			perror("Error: execve failed");
+			free(command_path);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		if (wait(&status) == -1)
+		{
+			perror("Error: wait failed");
+			free(command_path);
+			return (-1);
+		}
+		free(command_path);
+		if (WIFEXITED(status))
+			return (WEXITSTATUS(status));
+	}
+	return (-1);
 }
