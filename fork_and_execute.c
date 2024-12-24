1dfa36d0380d158cd84a8b179a0de2ec50ee2265
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-24 18:12:16 +0100

    fix fork condition

--- fork_and_execute.c
+++ fork_and_execute.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * forking_helper - Forks and executes a command
- * @command_path: Full path to the command
- * @args: Array of command arguments
+ * fork_and_execute - Forks a new process and executes a command
+ * @command_path: Full path to the command to be executed
+ * @args: NULL-terminated array of command arguments
  * @envp: Array of environment variables
  *
  * Return: 0 on success, -1 on failure
  */
-int forking_helper(char *command_path, char **args, char **envp)
+int fork_and_execute(char *command_path, char **args, char **envp)
 {
 	pid_t pid;
 	int status;
@@ -17,7 +17,6 @@ int forking_helper(char *command_path, char **args, char **envp)
 	if (pid == -1)
 	{
 		perror("Error: fork failed");
-		free(command_path);
 		return (-1);
 	}
 	else if (pid == 0)
@@ -25,7 +24,6 @@ int forking_helper(char *command_path, char **args, char **envp)
 		if (execve(command_path, args, envp) == -1)
 		{
 			perror("Error: execve failed");
-			free(command_path);
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -34,10 +32,8 @@ int forking_helper(char *command_path, char **args, char **envp)
 		if (wait(&status) == -1)
 		{
 			perror("Error: wait failed");
-			free(command_path);
 			return (-1);
 		}
-		free(command_path);
 		if (WIFEXITED(status))
 			return (WEXITSTATUS(status));
 	}
