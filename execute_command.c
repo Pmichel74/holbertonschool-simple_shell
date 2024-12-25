efe51490afd31bb3ea608b4cbbc226dab1064574
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-25 17:37:59 +0100

    synch

--- execute_command.c
+++ execute_command.c
@@ -10,9 +10,10 @@
  */
 int execute_command(char **args, char **envp, char *program_name)
 {
+	char *command_path = NULL;
 	char *command_path;
 	struct stat st;
-	int status;
+	int result;
 
 	if (!args || !args[0])
 		return (-1);
@@ -26,7 +27,13 @@ int execute_command(char **args, char **envp, char *program_name)
 	if (args[0][0] == '/' || args[0][0] == '.')
 	{
 		if (stat(args[0], &st) == 0 && st.st_mode & S_IXUSR)
+		{
 			command_path = strdup(args[0]);
+			if (!command_path)
+			{
+				perror("strdup");
+				return (-1);
+			}
 		else
 		{
 			fprintf(stderr, "%s: 1: %s: not found\n", program_name, args[0]);
@@ -42,12 +49,14 @@ int execute_command(char **args, char **envp, char *program_name)
 		return (-1);
 	}
 
-	status = fork_and_execute(command_path, args, envp);
-	if (status == -1)
+	result = forking_helper(command_path, args, envp);
+	if (fork_and_execute(command_path, args, envp) == -1)
 	{
-		fprintf(stderr, "%s: 1: %s: %s\n", program_name, args[0],
-		strerror(errno));
+		fprintf(stderr, "%s: 1: %s: %s\n", program_name, args[0], strerror(errno));
+		free(command_path);
+		return (-1);
 	}
 	free(command_path);
-	return (status);
+	return (result);
+	return (0);
 }
