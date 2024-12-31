e2e4a86466809ce3ebd20f3b4773824d5678ccd6
jbn179 <9772@holbertonstudents.com>
2024-12-31 10:35:25 +0100

    update main.c

--- main.c
+++ main.c
@@ -8,46 +8,61 @@
 *
 * Return: Always returns 0 (success)
 */
+
 int main(int argc __attribute__((unused)), char *argv[], char *envp[])
 {
-char *line = NULL;
-size_t len = 0;
-ssize_t nread;
-char **args;
-int interactive = isatty(STDIN_FILENO), status;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    char **args;
+    int interactive = isatty(STDIN_FILENO);
+    int last_status = 0;
 
-while (1)
-{
-if (interactive)
-printf("$ ");
-nread = getline(&line, &len, stdin);
-if (nread == -1)
-{
-if (interactive)
-printf("\n");
-break;
-}
-if (nread > 0 && line[nread - 1] == '\n')
-line[nread - 1] = '\0';
-if (strlen(line) == 0)
-continue;
-args = tokenize_command(line);
-if (!args || args[0] == NULL)
-{
-free_args(args);
-continue;
-}
-if (strcmp(args[0], "exit") == 0)
-{
-free_args(args);
-free(line);
-exit(EXIT_SUCCESS);
-}
-status = execute_command(args, envp, argv[0]);
-if (status == -1)
-fprintf(stderr, "%s: 1: %s: not found\n", argv[0], args[0]);
-free_args(args);
-}
-	free(line);
-return (0);
+    while (1)
+    {
+        if (interactive)
+            write(STDOUT_FILENO, "$ ", 2);
+
+        nread = getline(&line, &len, stdin);
+        if (nread == -1)
+        {
+            if (interactive)
+                write(STDOUT_FILENO, "\n", 1);
+            break;
+        }
+
+        if (nread > 0 && line[nread - 1] == '\n')
+            line[nread - 1] = '\0';
+
+        if (strlen(line) == 0)
+            continue;
+
+        args = tokenize_command(line);
+        if (!args || args[0] == NULL)
+        {
+            free_args(args);
+            continue;
+        }
+
+        if (strcmp(args[0], "exit") == 0)
+        {
+            int exit_status = last_status;
+            if (args[1] != NULL)
+            {
+                char *endptr;
+                exit_status = strtol(args[1], &endptr, 10);
+                if (*endptr != '\0')
+                    exit_status = 2;
+            }
+            free_args(args);
+            free(line);
+            exit(exit_status);
+        }
+
+        last_status = execute_command(args, envp, argv[0]);
+        free_args(args);
+    }
+
+    free(line);
+    return last_status;
 }
