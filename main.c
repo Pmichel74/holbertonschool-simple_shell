df647aa44f77b8ea9d88cf669dadb28525ccfd06
jbn179 <9772@holbertonstudents.com>
2024-12-31 12:51:32 +0100

    sync

--- main.c
+++ main.c
@@ -10,43 +10,44 @@
  */
 int main(int argc __attribute__((unused)), char *argv[], char *envp[])
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    char **args;
-    int interactive = isatty(STDIN_FILENO);
-    int last_status = 0;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	char **args;
+	int interactive = isatty(STDIN_FILENO);
+	int last_status = 0;
 
-    while (1)
-    {
-        if (interactive)
-            write(STDOUT_FILENO, "$ ", 2);
+	while (1)
+	{
+		if (interactive)
+			write(STDOUT_FILENO, "$ ", 2);
 
-        nread = getline(&line, &len, stdin);
-        if (nread == -1)
-        {
-            if (interactive)
-                write(STDOUT_FILENO, "\n", 1);
-            break;
-        }
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
+		{
+			if (interactive)
+				write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
 
-        if (nread > 0 && line[nread - 1] == '\n')
-            line[nread - 1] = '\0';
+		if (nread > 0 && line[nread - 1] == '\n')
+			line[nread - 1] = '\0';
 
-        if (strlen(line) == 0)
-            continue;
+		if (strlen(line) == 0)
+			continue;
 
-        args = tokenize_command(line);
-        if (!args || args[0] == NULL)
-        {
-            free_args(args);
-            continue;
-        }
+		args = tokenize_command(line);
+		if (!args || args[0] == NULL)
+		{
+			free_args(args);
+			continue;
+		}
 
-   if (strcmp(args[0], "exit") == 0)
-        {
-            int exit_status = last_status;
+		if (strcmp(args[0], "exit") == 0)
+		{
+			int exit_status = last_status;
 
+<<<<<<< HEAD
             if (args[1] != NULL)
             {
                 int parsed_status;
@@ -61,16 +62,32 @@ int main(int argc __attribute__((unused)), char *argv[], char *envp[])
                     exit_status = 2;
                 }
             }
+=======
+			if (args[1] != NULL)
+			{
+				int parsed_status;
+>>>>>>> f0752f7 (sync)
 
-            free_args(args);
-            free(line);
-            exit(exit_status);
-        }
+				if (string_to_int(args[1], &parsed_status) == 0)
+				{
+					exit_status = parsed_status;
+				}
+				else
+				{
+					fprintf(stderr, "%s: exit: Illegal number: %s\n", argv[0], args[1]);
+					exit_status = 2;
+				}
+			}
 
-        last_status = execute_command(args, envp, argv[0]);
-        free_args(args);
-    }
+			free_args(args);
+			free(line);
+			exit(exit_status);
+		}
 
-    free(line);
-    return (last_status);
+		last_status = execute_command(args, envp, argv[0]);
+		free_args(args);
+	}
+
+	free(line);
+	return (last_status);
 }
