73e82f938fa9082346224b91f0761e44612e3fa4
jbn179 <9772@holbertonstudents.com>
2024-12-31 19:53:41 +0100

    fixed custom_getline

--- custom_getline.c
+++ custom_getline.c
@@ -1,63 +1,64 @@
 #include "main.h"
 
-#define BUFFER_SIZE 1024
-
 /**
- * custom_getline - Read line from stdin
- * @lineptr: Pointer to the buffer that will hold the read line
- * @n: Size of the buffer
- * @stream: Input stream to read from (unused)
+ * custom_getline - Reads an entire line from stream
+ * @lineptr: Pointer to the buffer that will store the line
+ * @n: Pointer to the size of the buffer
+ * @stream: The stream to read from (unused)
  *
- * Return: Number of bytes read, or -1 on failure
+ * Return: Number of characters read, -1 on failure
  */
 ssize_t custom_getline(char **lineptr, size_t *n,
 FILE *stream __attribute__((unused)))
 {
-    static char buffer[BUFFER_SIZE];
-    static ssize_t pos;
-    static ssize_t chars_read;
-    ssize_t i;
-    char *new_ptr;
-    char c;
+    static char buffer[1024];
+    static int buf_size;
+    static int buf_pos;
+    int line_pos = 0;
+    char *temp;
 
-    if (lineptr == NULL || n == NULL)
+    if (!lineptr || !n)
         return (-1);
 
     if (*lineptr == NULL)
     {
-        *n = BUFFER_SIZE;
-        *lineptr = malloc(*n);
-        if (*lineptr == NULL)
+        *n = 128;
+        *lineptr = malloc(sizeof(char) * (*n));
+        if (!*lineptr)
             return (-1);
     }
 
-    i = 0;
     while (1)
     {
-        if (pos >= chars_read)
+        if (buf_pos == buf_size)
         {
-            chars_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
-            if (chars_read <= 0)
+            buf_size = read(STDIN_FILENO, buffer, 1024);
+            buf_pos = 0;
+            if (buf_size <= 0)
                 return (-1);
-            pos = 0;
         }
 
-        c = buffer[pos++];
-        if (i >= (ssize_t)(*n - 1))
+        if ((size_t)line_pos + 1 >= *n)
         {
-            new_ptr = malloc(*n * 2);
-            if (new_ptr == NULL)
-                return (-1);
-            memcpy(new_ptr, *lineptr, i);
-            free(*lineptr);
-            *lineptr = new_ptr;
             *n *= 2;
+            temp = malloc(*n);
+            if (!temp)
+                return (-1);
+            memcpy(temp, *lineptr, line_pos);
+            free(*lineptr);
+            *lineptr = temp;
         }
-        (*lineptr)[i++] = c;
 
-        if (c == '\n')
-            break;
+        (*lineptr)[line_pos] = buffer[buf_pos];
+
+        if (buffer[buf_pos] == '\n')
+        {
+            (*lineptr)[line_pos + 1] = '\0';
+            buf_pos++;
+            return (line_pos + 1);
+        }
+
+        line_pos++;
+        buf_pos++;
     }
-    (*lineptr)[i] = '\0';
-    return (i);
 }
