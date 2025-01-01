c5537a10083380f95cb4cf40e34f1b9fc6165dc3
jbn179 <9772@holbertonstudents.com>
2025-01-01 14:04:22 +0100

    fixed custom_getline

--- custom_getline.c
+++ custom_getline.c
@@ -11,44 +11,50 @@
 ssize_t custom_getline(char **lineptr, size_t *n,
 FILE *stream __attribute__((unused)))
 {
-	static char buffer[1024];
-	static int index;
-	static int bytes_read;
-	ssize_t count = 0;
+    static char buffer[1024];
+    static int index;
+    static int bytes_read;
+    ssize_t count = 0;
+    char *new_ptr;
 
-	if (!lineptr || !n)
-		return (-1);
+    if (!lineptr || !n)
+        return (-1);
 
-	if (*lineptr == NULL)
-	{
-		*n = 1024;
-		*lineptr = malloc(*n);
-		if (!*lineptr)
-			return (-1);
-	}
-	while (1)
-	{
-		if (index >= bytes_read)
-		{
-			index = 0;
-			bytes_read = read(0, buffer, 1024);
-			if (bytes_read <= 0)
-				return (-1);
-		}
+    if (*lineptr == NULL)
+    {
+        *n = 1024;
+        *lineptr = malloc(*n);
+        if (!*lineptr)
+            return (-1);
+    }
 
-		(*lineptr)[count++] = buffer[index++];
+    while (1)
+    {
+        if (index >= bytes_read)
+        {
+            index = 0;
+            bytes_read = read(0, buffer, 1024);
+            if (bytes_read <= 0)
+                return (-1);
+        }
 
-		if (count >= (ssize_t)(*n - 1))
-		{
-			*n *= 2;
-			*lineptr = realloc(*lineptr, *n);
-			if (!*lineptr)
-				return (-1);
-		}
+        if (count >= (ssize_t)(*n - 1))
+        {
+            new_ptr = malloc(*n * 2);
+            if (!new_ptr)
+                return (-1);
+            memcpy(new_ptr, *lineptr, count);
+            free(*lineptr);
+            *lineptr = new_ptr;
+            *n *= 2;
+        }
 
-		if ((*lineptr)[count - 1] == '\n')
-			break;
-	}
-	(*lineptr)[count] = '\0';
-	return (count);
+        (*lineptr)[count++] = buffer[index++];
+
+        if ((*lineptr)[count - 1] == '\n')
+            break;
+    }
+
+    (*lineptr)[count] = '\0';
+    return (count);
 }
