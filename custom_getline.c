b2e5378f78872ee538f13f151d4aa252897c75d0
Pmichel74 <patrick.yann.michelgmail.com>
2025-01-03 13:15:07 +0100

    update getline funt

--- custom_getline.c
+++ custom_getline.c
@@ -3,162 +3,81 @@
 #define READ_SIZE 1024
 
 /**
- * copy_and_reallocate - Reallocates memory for buffer
- * @ptr: Original buffer
- * @old_size: Old size
- * @new_size: New size
- * Return: Pointer to new buffer
- */
-static void *copy_and_reallocate(void *ptr, unsigned int old_size,
-unsigned int new_size)
-{
-    void *mem;
-    char *ptr_copy, *filler;
-    unsigned int index;
-
-    if (new_size == old_size)
-        return (ptr);
-
-    if (ptr == NULL)
-    {
-        mem = malloc(new_size);
-        if (mem == NULL) {
-            perror("malloc"); /* // Ajouté perror */
-            return (NULL);
-        }
-        return (mem);
-    }
-
-    if (new_size == 0 && ptr != NULL)
-    {
-        free(ptr);
-        return (NULL);
-    }
-
-    ptr_copy = ptr;
-    mem = malloc(new_size);
-    if (mem == NULL)
-    {
-        perror("malloc"); /* // Ajouté perror */
-        free(ptr);
-        return (NULL);
-    }
-
-    filler = mem;
-    for (index = 0; index < old_size && index < new_size; index++)
-        filler[index] = ptr_copy[index];
-
-    free(ptr);
-    return (mem);
-}
-
-/**
- * assign_line - Assigns buffer to lineptr
- * @lineptr: Buffer to store input
- * @n: Size of lineptr
- * @buffer: String to assign
- * @b: Size of buffer
- */
-static void assign_line(char **lineptr, size_t *n, char *buffer, size_t b)
-{
-    if (*lineptr == NULL || *n < b)
-    {
-        *n = b > READ_SIZE ? b : READ_SIZE;
-        *lineptr = buffer;
-    }
-    else
-    {
-        memcpy(*lineptr, buffer, b);
-        free(buffer);
-    }
-}
-
-/**
- * custom_getline - Read input from stream
- * @lineptr: Buffer to store input
- * @n: Size of buffer
- * @stream: Stream to read from
- * Return: Number of bytes read
+ * custom_getline - Reads a line from the specified stream.
+ * @lineptr: Pointer to the buffer to store the line.
+ * @n: Size of the buffer.
+ * @stream: Stream to read from (usually stdin).
+ *
+ * Return: The number of characters read, or -1 on failure or EOF.
  */
 ssize_t custom_getline(char **lineptr, size_t *n, FILE *stream)
 {
-    static char read_buffer[READ_SIZE];
-    static size_t buffer_pos;
-    static ssize_t chars_in_buffer;
-    char *line_buffer;
-    size_t line_pos = 0;
+	static char read_buffer[READ_SIZE];
+	static size_t buffer_pos = 0;
+	static ssize_t chars_in_buffer = 0;
+	size_t line_pos = 0;
 
-    if (!lineptr || !n)
-        return (-1);
+	/* Check if the arguments are valid */
+	if (!lineptr || !n || !stream)
+		return (-1);
 
-    line_buffer = malloc(READ_SIZE);
-    if (line_pos >= *n)
-{
-    *n = line_pos + READ_SIZE;
-    *lineptr = realloc(*lineptr, *n);
-    if (*lineptr == NULL)
-    {
-        perror("realloc");
-        return -1;
-    }
-}
+	/* If no buffer or insufficient size, allocate memory for lineptr */
+	if (*lineptr == NULL || *n < READ_SIZE)
+	{
+		*n = READ_SIZE;
+		*lineptr = malloc(*n);
+		if (*lineptr == NULL)
+		{
+			perror("malloc");
+			return (-1);
+		}
+	}
 
-    if (buffer_pos >= (size_t)chars_in_buffer)
-    {
-        fflush(stream);
-        chars_in_buffer = read(fileno(stream), read_buffer, READ_SIZE); /* // Correction : fileno(stream) */
-        if (chars_in_buffer <= 0)
-        {
-            free(line_buffer);
-            return (-1);
-        }
-        buffer_pos = 0;
-    }
+	/* Loop to read data into the buffer */
+	while (1)
+	{
+		/* If the buffer is empty, refill it from the stream */
+		if (buffer_pos >= (size_t)chars_in_buffer)
+		{
+			chars_in_buffer = read(fileno(stream), read_buffer, READ_SIZE);
+			if (chars_in_buffer < 0)
+			{
+				perror("read");
+				return (-1);
+			}
+			if (chars_in_buffer == 0) /* EOF */
+			{
+				if (line_pos == 0) /* If no data was read, return -1 */
+					return (-1);
+				break;
+			}
+			buffer_pos = 0;
+		}
 
-    while (1)
-    {
-        /* Copy characters until newline or buffer is full */
-        while (buffer_pos < (size_t)chars_in_buffer)
-        {
-            line_buffer[line_pos++] = read_buffer[buffer_pos++];
+		/* Copy the character from the read buffer to lineptr */
+		(*lineptr)[line_pos++] = read_buffer[buffer_pos++];
 
-            /* Check for newline */
-            if (line_buffer[line_pos - 1] == '\n')
-            {
-                line_buffer[line_pos] = '\0';
-                assign_line(lineptr, n, line_buffer, line_pos + 1);
-                return (line_pos);
-            }
+		/* If we hit a newline, terminate the string and return */
+		if ((*lineptr)[line_pos - 1] == '\n')
+		{
+			(*lineptr)[line_pos] = '\0';
+			return (line_pos);
+		}
 
-            /* Reallocate if needed */
-            if (line_pos >= READ_SIZE) {
-                line_buffer = copy_and_reallocate(line_buffer, line_pos, line_pos + READ_SIZE);
-                if (line_buffer == NULL) { /* // Vérification après réallocation */
-                    perror("realloc"); /* // Ajouté perror */
-                    return -1;
-                }
-            }
-        }
+		/* If the buffer is full, reallocate more space */
+		if (line_pos >= *n)
+		{
+			*n += READ_SIZE;
+			*lineptr = realloc(*lineptr, *n);
+			if (*lineptr == NULL)
+			{
+				perror("realloc");
+				return (-1);
+			}
+		}
+	}
 
-        /* Read more data if needed */
-        chars_in_buffer = read(fileno(stream), read_buffer, READ_SIZE); /* // Correction : fileno(stream) */
-        if (chars_in_buffer <= 0) {
-            if (chars_in_buffer < 0) {
-                perror("read"); /* // Ajouté perror */
-            }
-            break;
-        }
-        buffer_pos = 0;
-    }
-
-    /* Handle end of input*/
-    if (line_pos == 0)
-    {
-        free(line_buffer);
-        return (-1);
-    }
-
-    line_buffer[line_pos] = '\0';
-    assign_line(lineptr, n, line_buffer, line_pos + 1);
-    return (line_pos);
+	/* Add the null terminator */
+	(*lineptr)[line_pos] = '\0';
+	return (line_pos);
 }
