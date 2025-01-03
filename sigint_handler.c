36ca2789f38e2dc03ed5cae9783f87c5fdb23144
jbn179 <9772@holbertonstudents.com>
2025-01-03 12:38:24 +0100

    fixed sigint_handler

--- sigint_handler.c
+++ sigint_handler.c
@@ -1,18 +1,5 @@
 #include "main.h"
 
-#define BUF_FLUSH -1
-
-/**
- * _puts - prints a string
- * @str: pointer to the string
- *
- * Return: number of chars written
- */
-static int _puts(const char *str)
-{
-	return (write(STDOUT_FILENO, str, strlen(str)));
-}
-
 /**
  * sigint_handler - blocks ctrl-C
  * @sig_num: the signal number
@@ -22,6 +9,6 @@ static int _puts(const char *str)
 void sigint_handler(int sig_num)
 {
 	(void)sig_num;
-	_puts("\n$ ");
+	write(STDOUT_FILENO, "\n$ ", 3);
 	fflush(stdout);
 }
